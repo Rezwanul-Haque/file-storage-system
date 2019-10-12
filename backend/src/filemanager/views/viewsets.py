677023d99098df7609f807463d4c7cea20390b5c
Rezwanul-Haque <rezwanul.cse@gmail.com>
2019-10-13 01:56:27 +0600

    viewsets post and destroy method added.

--- backend/src/filemanager/views/viewsets.py
+++ backend/src/filemanager/views/viewsets.py
@@ -1,7 +1,37 @@
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
 from rest_framework import viewsets
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+
 from filemanager.models.file_data import Data
 from filemanager.serializers.file_serializers import DataSerializer
 
 class DataViewSet(viewsets.ModelViewSet):
     queryset = Data.objects.all()
     serializer_class = DataSerializer
+
+    permission_classes = (permissions.AllowAny,) # session user
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request,  format=None):
+        serializer = DataSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, *args, **kwargs):
+        try:
+            for k, v in kwargs.items():
+                for id in v.split(','):
+                    obj = get_object_or_404(Data, pk=int(id))
+                    self.perform_destroy(obj)
+        except Http404:
+            pass
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
